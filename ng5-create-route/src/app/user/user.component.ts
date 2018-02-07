62fd6b80539c0a8b1abfe62a82c5ae9cc837965a
Julia Chen <juliachenjing@gmail.com>
2018-02-07 15:35:47 -0500

    finished

--- ng5-create-route/src/app/user/user.component.ts
+++ ng5-create-route/src/app/user/user.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  name:any;
+  sub: any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+  this.sub =  this.route.params.subscribe(params => {
+       this.name = params['name'];
+    });
+  }
+  ngOnDistroy(){
+    this.sub.unsubscribe();
   }
 
 }
